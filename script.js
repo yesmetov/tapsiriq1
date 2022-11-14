ef91847ce5d56b163df6524da7d6b6c870a4718b
yesmetov <114507946+yesmetov@users.noreply.github.com>
2022-11-14 18:56:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function isValidNumber(number) {
+    if (number.length[0] == '+' && number.length[1] == 7) {
+        console.log('True');
+    } else {
+        console.log('False');
+    }
+}
+
+isValidNumber(+72345678912);
